7a1c511aa7bf28933880ebab37c051b02be95d07
Ionita Mihnea <ionitamihneadjgogu@gmail.com>
2018-09-17 23:02:53 +0300

    ok

--- Project2/script.js
+++ Project2/script.js
@@ -1,5 +1,5 @@
 var firstName = prompt("What's your first name?");
 var lastName = prompt("What's your last name?");
 var age = prompt("What's your age?");
-alert("Your full name is " + firstName + " " +lastName);
+alert("Your full name is " + firstName + " " + lastName);
 alert("You are " + age + " years old.");
