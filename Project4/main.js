4f1fc979eb97717bc0e414a625ea8262c25a1111
Ionita Mihnea <ionitamihneadjgogu@gmail.com>
2018-09-19 16:31:04 +0300

    For loops

---
+++ Project4/main.js
@@ -0,0 +1,34 @@
+console.log("Prima");
+var x = -10;
+while (x<20)
+{
+    console.log(x);
+    x++;
+}
+console.log("A doua");
+var y = 10;
+while (y<41)
+{
+    if(y%2===0){
+    console.log(y);
+    }
+    y++;
+}
+console.log("A treia");
+var z = 300;
+while (z<=333)
+{
+    if(z % 2 !== 0){
+    console.log(z);
+    }
+    z++;
+}
+console.log("A patra");
+var a = 5;
+while (a<51)
+{
+    if(a%5==0 && a%3==0) {
+    console.log(a);
+    }
+    a++;
+}
