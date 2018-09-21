0497f275f1ed4f22cddef18b3ef56a98613aaf32
Ionita Mihnea <ionitamihneadjgogu@gmail.com>
2018-09-21 16:04:10 +0300

    update todos

--- Project7/main.js
+++ Project7/main.js
@@ -3,12 +3,32 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit") {
     if (input === "list") {
-        console.log(todos);
+        listTodos();
     }
     else if (input === "new") {
-        var newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
+        AddTodo();
+    }
+    else if (input === "delete"){
+        removeTodo();
     }
     input = prompt("What would you like to do?");
 }
+
+function listTodos(){
+    console.log("***********");
+    todos.forEach(function(todo, i){
+    console.log(i + ": ", todo)});
+    console.log("***********");
+}
+
+function AddTodo(){
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+}
+
+function removeTodo(){
+    var index = prompt("Enter index of todo to delete");
+    todos.splice(index,1);
+}
+
 console.log("BYE");
